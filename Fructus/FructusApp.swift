1f67addec97d70972a1f5bc8cb526af3f2d77e33
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-11 15:44:21 +0200

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Cristina on 2023-07-11.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
