6f6a254fb301271253ad23db8c7ef55f4b078a2e
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-12 21:16:57 +0200

    add StackNavigationViewStyle for adaptation to bigger screens

--- Fructus/App/FruitDetailView.swift
+++ Fructus/App/FruitDetailView.swift
@@ -61,6 +61,7 @@ struct FruitDetailView: View {
             } //: SCROLL
             .edgesIgnoringSafeArea(.top)
         } //: NAVIGATION
+        .navigationViewStyle(StackNavigationViewStyle()) // adapting the navigation view to Ipad bigger screen
     }
 }
 
