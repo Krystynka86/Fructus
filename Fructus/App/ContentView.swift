8b9aa046b0dc6e3a608482ae168f9e6380f8e85c
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-12 14:43:52 +0200

    add NavigationLink to ContentView

--- Fructus/App/ContentView.swift
+++ Fructus/App/ContentView.swift
@@ -17,8 +17,11 @@ struct ContentView: View {
         NavigationView {
             List {
                 ForEach(fruits.shuffled()) { item in
-                    FruitRowView(fruit: item)
+                    NavigationLink(destination: FruitDetailView(fruit: item)) {
+                        FruitRowView(fruit: item)
                         .padding(.vertical, 4)
+                        
+                    }
                     
                 }
             }
