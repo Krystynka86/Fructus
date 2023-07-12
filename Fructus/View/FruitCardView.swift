0e8da0de993239ba3e425cab2a1b9252f169b7c3
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-12 11:23:47 +0200

    fetch data to Fruit Card

--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct FruitCardView: View {
     // MARK: - PROPERTIES
     
+    var fruit: Fruit
+    
     @State private var isAnimating: Bool = false
    
     
@@ -20,7 +22,7 @@ struct FruitCardView: View {
         ZStack {
             VStack(spacing: 20) {
                 // FRUIT: IMAGE
-                Image("blueberry")
+                Image(fruit.image)
                     .resizable()
                     .scaledToFit()
                     .shadow(color: Color(red: 0, green: 0, blue: 0,
@@ -29,7 +31,7 @@ struct FruitCardView: View {
                     
                 
                 // FRUIT: TITLE
-                Text("Blueberry")
+                Text(fruit.title)
                     .foregroundColor(Color.white)
                     .font(.largeTitle)
                     .fontWeight(.heavy)
@@ -37,7 +39,7 @@ struct FruitCardView: View {
                         opacity: 0.15), radius: 2, x: 2, y: 2)
                 
                 // FRUIT: HEADLINE
-                Text("Bluberries are sweet, nutrious and widly popular fruit all over the world.")
+                Text(fruit.headline)
                     .foregroundColor(Color.white)
                     .multilineTextAlignment(.center)
                     .padding(.horizontal, 16)
@@ -56,8 +58,7 @@ struct FruitCardView: View {
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0,
             maxHeight: .infinity, alignment: .center)
-        .background(LinearGradient(gradient: Gradient(colors:
-            [Color("ColorBlueberryLight"),Color("ColorBlueberryDark")]),
+        .background(LinearGradient(gradient: Gradient(colors: fruit.gradientColors),
             startPoint: .top, endPoint: .bottom))
         .cornerRadius(20)
         .padding(.horizontal, 20)
@@ -66,7 +67,7 @@ struct FruitCardView: View {
 
 struct FruitCardView_Previews: PreviewProvider {
     static var previews: some View {
-        FruitCardView()
+        FruitCardView(fruit: fruitsData[2])
             .previewLayout(.fixed(width: 320, height: 640))
     }
 }
